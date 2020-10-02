37175e0df6d4820c7a3ec99598b1beacd8924500
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2020-10-02 15:02:49 +0100

    added 1 and 10 year data extraction

--- server/server.js
+++ server/server.js
@@ -26,12 +26,12 @@ app.get('/', function(req, res){
             data = data.replace(/\)/g, '');
             data = data.substring(3, data.length-2) + ']'
             data = JSON.parse(data)
-            console.log(data)
+            //console.log(data)
 
             // create array of objects
             data = data.map(x => ({
                 year: Number(x[0]),
-                month: Number(x[1]) + 1,
+                month: Number(x[1]),
                 day: Number(x[2]),
                 ppm: Number(x[3]),
                 week: moment(new Date(x[0], x[1], x[2])).week(),
@@ -45,48 +45,60 @@ app.get('/', function(req, res){
             // define final data
             var finalData = [];
 
-            // get last five days of data
-            var latestData = data.slice(data.length-5)
+            // get last 7 days of data
+            var latestData = data.slice(data.length-7)
+            console.log(latestData)
             // get all years in latest data (accounts for end/start of years)
-            var years = latestData.map(x => x.year);
-            var years = [...new Set(years)];
-            //console.log(latestData)
+            // var years = latestData.map(x => x.year);
+            // var years = [...new Set(years)];
+            
 
             finalData.push(latestData);
-            
+            console.log(finalData)
             // daily data begins on 11/05/2016
             // weekly data begins on 29/02/1958
             // yearly data begins 1830
             // 5 years data before 1830
 
-            //create week id for any data beyond 29/02/1958
+
 
             // Get data for 1, 10, 100, and 1000 years ago
             
             // 1 year ago
             // generate ids matching data from last year based on latest data
             var ids = latestData.map((x) => (x.year-1)  + 
-            '_' + (x.month) + '_' + x.day)
+                            '_' + (x.month) + '_' + x.day)
             
             // pull new last year ids out of data and add to latestData
-            finalData.push(data.filter(x => ids.includes(x.ymd)));
-            //console.log(test)
-            //console.log(ids)
+            var temp = data.filter(x => ids.includes(x.ymd));
+            finalData.push(temp);
+            
             
             // 10 years ago
             // data now weekly so match nearest week based on latest day
             // find year and month
             // get latest day
             var latestDay = data.slice(data.length-1)
-            var yearMonth = (latestDay[0].year-10) + '_' + latestDay[0].month
-            console.log(yearMonth)
+            //console.log(latestDay[0]. -1)
+            var temp = data.filter(x => x.year == (latestDay[0].year - 10))
+                    .filter(x => x.week == latestDay[0].week)
+            finalData.push(temp)
+            console.log(finalData)
+            
+            // finalData.push(data.filter(x => x.year = (latestDay[0].year - 10))
+            //     .filter(x => x.week = latestDay.week)
+            // )
+            //console.log(finalData)
+            
+            // var yearMonth = (latestDay[0].year-10) + '_' + latestDay[0].month
+            // //console.log(yearMonth)
 
-            // in data find week starts for year-month
-            var weekStart = data.filter(x => x.ym == yearMonth).map((x)=> x.day)
-            console.log(weekStart)
-            // given latestDay find equivalent week from 10 years ago by matching to closest number below
-            var num = latestDay[0].day;
-            console.log(num)
+            // // in data find week starts for year-month
+            // var weekStart = data.filter(x => x.ym == yearMonth).map((x)=> x.day)
+            // //console.log(weekStart)
+            // // given latestDay find equivalent week from 10 years ago by matching to closest number below
+            // var num = latestDay[0].day;
+            //console.log(num)
             // subtract each weekStart from num and choose smallest positive difference
             //var test = Math.min(weekStart.map((x) => num-x).filter(x >= 0))
             
